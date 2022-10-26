575c9c3b93d115a0d47e97e9f77183af613c8bdf
davi38 <davi4alves@gmail.com>
2022-10-25 22:40:21 -0300

    feat: db employee route

--- src/main/java/com/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -1,17 +1,34 @@
 package com.employee.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.employee.model.Employee;
+import com.employee.repository.EmployeeRepository;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
 	
-
-	@GetMapping("/")
+	@GetMapping("/hi")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
 		
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	// get all employees
+	
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		return employeeRepository.findAll();
+	}
+	
+	
 }
--- src/main/java/com/employee/model/Employee.java
+++ src/main/java/com/employee/model/Employee.java
@@ -1,23 +1,44 @@
 package com.employee.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="employee")
 public class Employee {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
+	@Column(name="first_name")
 	private String firstName;
+	
+	@Column(name="last_name")
 	private String lastName;
+	
+	@Column(name="email_id")
 	private String emailId;
 
 	public Employee() {
 
 	}
+
+	
+	
+	public Employee(long id, String firstName, String lastName, String emailId) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+	}
+
+
 
 	public long getId() {
 		return id;

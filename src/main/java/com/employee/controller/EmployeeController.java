76ab8239d35232af460c3ff29bff8e4bc8e56fa0
davi38 <davi4alves@gmail.com>
2022-10-26 02:35:54 -0300

    feat: add new route for checking by id

--- src/main/java/com/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -3,32 +3,50 @@ package com.employee.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.employee.exception.ResourceNotFoundException;
 import com.employee.model.Employee;
 import com.employee.repository.EmployeeRepository;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
-	
+
 	@GetMapping("/hi")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
-		
-	
+
 	@Autowired
 	private EmployeeRepository employeeRepository;
-	
+
 	// get all employees
-	
 	@GetMapping("/employees")
-	public List<Employee> getAllEmployees(){
+	public List<Employee> getAllEmployees() {
 		return employeeRepository.findAll();
 	}
-	
-	
+
+	// create Employee rest api
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+	}
+
+	// get employee by id rest api
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+		Employee employee = employeeRepository.findById(id).orElseThrow(
+				()-> new ResourceNotFoundException("Employee not exist with id" + id));
+				return ResponseEntity.ok(employee);
+	}
+
 }
